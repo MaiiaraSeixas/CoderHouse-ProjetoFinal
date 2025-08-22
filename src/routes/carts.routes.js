0c30a5269e05fb64f4d254c7f913758351fca96d
MaiiaraSeixas <maiarasmendes21@gmail.com>
2025-08-22 14:27:06 -0300

    Melhora de nomeclatura

--- src/routes/carts.routes.js
+++ src/routes/carts.routes.js
@@ -8,58 +8,58 @@ const router = Router();
 
 // Rota para finalizar a compra
 router.post('/:cid/purchase',
-  passport.authenticate('jwt', { session: false }),
-  handlePolicies(['USER', 'PREMIUM']),
-  cartController.purchaseCart
+	passport.authenticate('jwt', { session: false }),
+	handlePolicies(['USER', 'PREMIUM']),
+	cartController.purchaseCart
 );
 
 // Rota para obter todos os carrinhos (Admin)
 router.get('/',
-  passport.authenticate('jwt', { session: false }),
-  handlePolicies(['ADMIN']),
-  cartController.getAllCarts
+	passport.authenticate('jwt', { session: false }),
+	handlePolicies(['ADMIN']),
+	cartController.getAllCarts
 );
 
 // Rota para obter um carrinho por ID
 router.get('/:cid',
-  passport.authenticate('jwt', { session: false }),
-  handlePolicies(['USER', 'ADMIN', 'PREMIUM']),
-  cartController.getCartById
+	passport.authenticate('jwt', { session: false }),
+	handlePolicies(['USER', 'ADMIN', 'PREMIUM']),
+	cartController.getCartById
 );
 
 // Rota para criar um novo carrinho
 router.post('/',
-  passport.authenticate('jwt', { session: false }),
-  handlePolicies(['USER', 'ADMIN', 'PREMIUM']),
-  cartController.createCart
+	passport.authenticate('jwt', { session: false }),
+	handlePolicies(['USER', 'ADMIN', 'PREMIUM']),
+	cartController.createCart
 );
 
 // Rota para adicionar um produto ao carrinho
 router.post('/:cid/product/:pid',
-  passport.authenticate('jwt', { session: false }),
-  handlePolicies(['USER', 'PREMIUM']),
-  cartController.addProductToCart
+	passport.authenticate('jwt', { session: false }),
+	handlePolicies(['USER', 'PREMIUM']),
+	cartController.addProductToCart
 );
 
 // Rota para atualizar a quantidade de um produto
 router.put('/:cid/product/:pid',
-  passport.authenticate('jwt', { session: false }),
-  handlePolicies(['USER', 'PREMIUM']),
-  cartController.updateProductQuantityInCart
+	passport.authenticate('jwt', { session: false }),
+	handlePolicies(['USER', 'PREMIUM']),
+	cartController.updateProductQuantityInCart
 );
 
 // Rota para remover um produto específico do carrinho
 router.delete('/:cid/product/:pid',
-  passport.authenticate('jwt', { session: false }),
-  handlePolicies(['USER', 'PREMIUM']),
-  cartController.removeProductFromCart
+	passport.authenticate('jwt', { session: false }),
+	handlePolicies(['USER', 'PREMIUM']),
+	cartController.removeProductFromCart
 );
 
 // Rota para LIMPAR (esvaziar) todos os produtos do carrinho
 router.delete('/:cid',
-  passport.authenticate('jwt', { session: false }),
-  handlePolicies(['USER', 'PREMIUM']),
-  cartController.clearCart // Corrigido para usar clearCart, que é mais seguro
+	passport.authenticate('jwt', { session: false }),
+	handlePolicies(['USER', 'PREMIUM']),
+	cartController.clearCart // Corrigido para usar clearCart, que é mais seguro
 );
 
 export default router;
